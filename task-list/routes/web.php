6c7890568025551a16d431ef268655496b5146b2
CELIA <infinite010750@gmail.com>
2025-07-04 08:58:01 +0800

    Day2-練習1: 命名路由與導向練習

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'Hello Ruru！🎉'; #Ruru可以改成你的名字
 });
+
+Route::get('/welcome', function () {
+    return 'Welcome, Laravel!';
+}) -> name('welcome.page');
+
+Route::get('/start', function () {
+    return redirect()->route('welcome.page');
+});
+
+
+
