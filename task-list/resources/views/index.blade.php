61bbada47b111a9624ac6d6e1f7ebf072e2b8b27
CELIA <infinite010750@gmail.com>
2025-07-07 15:50:06 +0800

    Day3-練習2: 任務詳情頁

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -1,8 +1,18 @@
-@forelse ($tasks as $task)
-  <div>
-    <h2>{{ $task->title }}</h2>
-    <p>{{ $task->description }}</p>
-  </div>
-@empty
-  <p>No tasks available.</p>
-@endforelse
+{{-- 引入模板 --}}
+@extends('layouts.app')
+@section('title', 'The list of the books')
+@section('content')
+    <div>
+        @if (count($tasks))
+            @foreach ($tasks as $task)
+                <div>
+                    <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+                        {{ $task->title }}
+                    </a>
+                </div>
+            @endforeach
+        @else
+            <div>There are no tasks</div>
+        @endif
+    </div>
+@endsection
---
+++ task-list/resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>Task List APP</title>
+  </head>
+  <body>
+      <h1>@yield('title')</h1>
+      <div>@yield('content')</div>
+  </body>
+</html>
